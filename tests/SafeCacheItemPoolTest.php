2b9f3321485524fb30219f9a0bb9eb8fc2aa47a0
James Hayward <jhayward1980@gmail.com>
2018-05-27 21:39:43 +0100

    Trying to make the tests better, not sure if I'm succeeding!

--- tests/SafeCacheItemPoolTest.php
+++ tests/SafeCacheItemPoolTest.php
@@ -1,24 +1,70 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use GeeksAreForLife\SafeCache\SafeCacheItemPool;
-use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 class SafeCacheItemPoolTest extends TestCase
 {
     protected $safeCache;
 
-    public function setUp()
+    protected $backupPrefix = 'SafeCacheNoExpire.';
+
+    public function getEmptyCacheMock($itemCallback)
     {
-        // use symfony's in-memory cache
-        $cache = new ArrayAdapter();
-        $cache->clear();
-        $this->safeCache = new SafeCacheItemPool($cache);
+        $cache = $this->createMock(CacheItemPoolInterface::class);
+
+        $cache->method('getItem')
+              ->will($this->returnCallback($itemCallback));
+
+        return $cache;
     }
 
-    public function testGetSafeCacheItem()
+    public function getCacheItem($key, $existing = false)
     {
-        $item = $this->safeCache->getItem("test");
+        $item = $this->createMock(CacheItemInterface::class);
+
+        $item->method('getKey')
+             ->will($this->returnValue($key));
+
+        $item->method('isHit')
+             ->will($this->returnValue($existing));
+
+        return $item;
+    }
+
+    public function testGetNewSafeCacheItem()
+    {
+        // set up our mock cache pool and items for this test
+        $itemMock = function($key) {
+            $item = $this->getCacheItem($key, false);
+
+            // check we are doing what we expect
+            // pretty hacky though!
+            if (strpos($key, $this->backupPrefix) === false) {
+                $item->expects($this->once())
+                     ->method('expiresAfter')
+                     ->with($this->greaterThan(0));
+            }
+
+            return $item;
+        };
+
+        $cache = $this->getEmptyCacheMock($itemMock);
+
+        $cache->expects($this->exactly(2))
+              ->method('getItem')
+              ->withConsecutive(
+                ['test'],
+                [$this->backupPrefix . 'test']
+              );
+
+        $safeCache = new SafeCacheItemPool($cache);
+
+        $item = $safeCache->getItem("test");
         $this->assertTrue(is_a($item, 'GeeksAreForLife\SafeCache\SafeCacheItem'));
+        $this->assertSame($item->getPrimary()->getKey(), 'test');
+        $this->assertSame($item->getBackup()->getKey(), $this->backupPrefix . 'test');
     }
 }
